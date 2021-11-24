e91b567aa062255551b7de56fe0bee7e5008a6bb
susmitmishra125 <susmitmishra125@gmail.com>
2021-11-24 19:03:56 +0530

    update main for ram usuage

--- main.py
+++ main.py
@@ -61,50 +61,32 @@ def answer_question(question, answer_text, model, tokenizer):
     answer), and identifies the words within the answer_text that are the
     answer. Prints them out.
     '''
-    # ======== Tokenize ========
-    # Apply the tokenizer to the input text, treating them as a text-pair.
     input_ids = tokenizer.encode(question, answer_text)
-    # Report how long the input sequence is.
-    # ======== Set Segment IDs ========
-    # Search the input_ids for the first instance of the `[SEP]` token.
+.
     sep_index = input_ids.index(tokenizer.sep_token_id)
-    # The number of segment A tokens includes the [SEP] token istelf.
     num_seg_a = sep_index + 1
-    # The remainder are segment B.
     num_seg_b = len(input_ids) - num_seg_a
-    # Construct the list of 0s and 1s.
     segment_ids = [0]*num_seg_a + [1]*num_seg_b
-    # There should be a segment_id for every input token.
     assert len(segment_ids) == len(input_ids)
-    # ======== Evaluate ========
-    # Run our example through the model.
     outputs = model(torch.tensor([input_ids]),  # The tokens representing our input text.
                     # The segment IDs to differentiate question from answer_text
                     token_type_ids=torch.tensor([segment_ids]),
                     return_dict=True)
     start_scores = outputs.start_logits
     end_scores = outputs.end_logits
-    # ======== Reconstruct Answer ========
-    # Find the tokens with the highest `start` and `end` scores.
     answer_start = torch.argmax(start_scores)
     answer_end = torch.argmax(end_scores)
-    # Get the string versions of the input tokens.
     tokens = tokenizer.convert_ids_to_tokens(input_ids)
-    # Start with the first token.
     answer = tokens[answer_start]
-    # Select the remaining answer tokens and join them with whitespace.
     for i in range(answer_start + 1, answer_end + 1):
-        # If it's a subword token, then recombine it with the previous token.
         if tokens[i][0:2] == '##':
             answer += tokens[i][2:]
-        # Otherwise, add a space then the token.
         else:
             answer += ' ' + tokens[i]
-    # print('Answer: "' + answer + '"')
     return (answer, start_scores[0][answer_start], end_scores[0][answer_end])
 
 
-def query(question, text_sent, model, tokenizer, n_sent=1):
+def query(question, text_sent, model, tokenizer, n_sent=3):
     outputs = []
     sent_count = len(text_sent)
     ans = 0
@@ -115,15 +97,14 @@ def query(question, text_sent, model, tokenizer, n_sent=1):
         for sent in segment:
             passage += ' '.join(sent)+'. '
         output = answer_question(question, passage, model, tokenizer)
-        outputs.append(
-            (output[0], 2*(output[1]*output[2])/(output[1]+output[2])))
-        if 2*(output[1]*output[2])/(output[1]+output[2]) >= ans:
-            ans = 2*(output[1]*output[2])/(output[1]+output[2])
-            ans_text = output[0]
-    outputs.sort(key=lambda x: x[1], reverse=True)
-    # print('Top 10',outputs[:10])
+
+        outputs.append((output[0], 2*(output[1]*output[2])/(output[1]+output[2])))
+        if(len(outputs)>10):
+            outputs.sort(key=lambda x: x[1], reverse=True)
+            outputs=outputs[:10]
+
     cnt = Counter()
-    for output in outputs[:10]:
+    for output in outputs:
         cnt[output[0]] += 1
     return cnt.most_common()[0][0]
 
@@ -170,4 +151,3 @@ def traditional():
     # print(sentence_vectors.toarr)
 # traditional()
 modern()
-
